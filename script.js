c949eb256997888555fd276ba18ca4fe1a13486d
thecrownedsquid(nat) <natnatdap@gmail.com>
2017-02-01 20:31:52 +0000

    hi tinyturtle

---
+++ script.js
@@ -0,0 +1,19 @@
+/*global TinyTurtle*/
+/*golobal forward*/
+/*global stamp*/
+/*global right*/
+/*global left*/
+TinyTurtle.apply(window);
+/*stamp*/
+/*down*/
+/*left*/
+/*right*/
+forward(20);
+right(67);
+forward(28)
+right(45)
+forward(27) 
+right(67)
+forward(20)
+right(90)
+forward(50)
